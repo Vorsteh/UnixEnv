3032483831673ade05b3ccc129324bb2609e58e3
= <whyzoid@gmail.com>
2025-10-15 09:47:18 +0200

    Most of main is done.
    TODO: Implement structs to handle traversing and functions to calculate size.

--- OU3/mdu.c
+++ OU3/mdu.c
@@ -1,3 +1,4 @@
+
 #include <errno.h>
 #include <getopt.h>
 #include <stdbool.h>
@@ -7,4 +8,38 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
+int main(int argc, char *argv[]) {
+
+  int num_threads = 1;
+
+  int c;
+  while ((c = getopt(argc, argv, "f:")) != -1) {
+    switch (c) {
+    case 'j': {
+      // Convert and check that arguemnts are valid
+      char *end;
+      long value = strtol(optarg, &end, 10);
+      if (end == optarg || value <= 0) {
+        fprintf(stderr, "Invalid thread count for -j: %s\n", optarg);
+        return EXIT_FAILURE;
+      }
+
+      num_threads = (int)value;
+      break;
+    }
+    default:
+      fprintf(stderr, "Usage: %s [-j antal_trådar] fil ...\n", argv[0]);
+    }
+  }
+
+  // Check that usage is correct
+  if (optind >= argc) {
+    fprintf(stderr, "Usage: %s [-j antal_trådar] fil ...\n", argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  for (int i = optind; i < argc; ++i) {
+  }
+
+  return EXIT_SUCCESS;
+}
