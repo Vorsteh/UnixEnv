1fb4a0f8463de34219298f7cb971b745f86e9058
= <whyzoid@gmail.com>
2025-10-15 09:01:57 +0200

    Initial commit

---
+++ OU3/mdu.c
@@ -0,0 +1,10 @@
+#include <errno.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
